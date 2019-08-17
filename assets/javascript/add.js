7984c3eabc6c45aee90763a83a6d085a43baa9ef
sinuhe Montero <sinuhem23@gmail.com>
2019-08-17 14:09:32 -0400

    TriviaGame

---
+++ assets/javascript/add.js
@@ -0,0 +1,152 @@
+//test to see if document link is working
+console.log(quizQuestions);
+var counter = 15; // var that holds our counter
+var timer; // holds the timer for the game
+var currentQuestion = 0;
+var score = 0; //score var
+var lost = 0; //lost var
+
+
+//////////////////////////////////////////////////////////////////////
+
+
+// if the timer is over go to the next question
+function nextQuestion() {
+  
+    clearInterval(timer);
+  var isQuestionOver = quizQuestions.length - 1 === currentQuestion;
+
+  if (isQuestionOver) {
+    console.log("Game is over");
+    displayResults();
+  } else {
+    currentQuestion++;
+    loadQuestion();
+  }
+ 
+}
+
+/////////////////////////////////////////////////////////////////
+
+
+// when time is up clear timer and move to the next question and increase lost by 1
+function timeUp() {
+  clearInterval(timer);
+  lost++;
+  //   preloadImage("correctAnswer");
+  nextQuestion();
+}
+
+//start a 30 second timer for user to respond to each question
+
+function countDown() {
+  counter--;
+  $("#time").html("<h2> Time Left: " + counter + "</h2>");
+
+  if (counter === 0) {
+    timeUp();
+  }
+}
+
+//////////////////////////////////////////////////////////////////
+
+// Display the questions and choices to the browser
+function loadQuestion() {
+  clearInterval(timer);
+  counter = 15;
+  timer = setInterval(countDown, 1000);
+
+  
+  var question = quizQuestions[currentQuestion].question; //
+  var choices = quizQuestions[currentQuestion].choices; //
+
+  $("#time").html("<h2> Time Left: " + counter + "</h2>");
+
+  //append to id game
+  $("#game").html(`
+    <h4> ${question} </h4>
+    ${loadChoices(choices)}
+    ${loadRemainingQuestions()}
+    `);
+}
+
+// load question function
+function loadChoices(choices) {
+  var result = "";
+
+  for (var i = 0; i < choices.length; i++) {
+    result += `<button class=" btn btn-primary choice button-color" data-answer="${
+      choices[i]
+    }">${choices[i]}</button>`;
+  }
+
+  return result;
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+
+
+// Either wrong and right answer selected, go to the next question
+
+$(document).on("click", ".choice", function() {
+  //   clearInterval(timer);
+  var selectedAnswer = $(this).attr("data-answer");
+  var correctAnswer = quizQuestions[currentQuestion].correctAnswer;
+  // check if working
+  // console.log("yes", selectedAnswer);
+
+  if (correctAnswer === selectedAnswer) {
+    console.log("yes");
+    score++;
+    nextQuestion();
+  } else {
+    lost++;
+    console.log("no");
+    nextQuestion();
+  }
+});
+
+////////////////////////////////////////////////////////////////////////////////
+
+// display the results
+
+function displayResults() {
+  var result = `
+<p> You get ${score} questions(s) right</p>
+<p> You missed ${lost} questions(s) right</p>
+<p> Total questions ${quizQuestions.length} questions(s) right</p>
+<button class="btn btn-primary button-color" id="reset"> Reset Game </button>
+    `;
+  $("#game").html(result);
+  reset();
+}
+
+function reset() {
+  $(document).on("click", "#reset", function() {
+    console.log("reset clicked");
+    counter = 5; // var that holds our counter
+    timer = null; // holds the timer for the game
+    currentQuestion = 0;
+    score = 0; //score var
+    lost = 0; //lost var
+
+    loadQuestion();
+  });
+}
+
+function loadRemainingQuestions() {
+  var remainingQuestion = quizQuestions.length - (currentQuestion + 1);
+  var totalQuestion = quizQuestions.length;
+
+  // returns remaining questions into the page with p tag
+  return `<br><br>  <p>Remaining Question: ${remainingQuestion}/${totalQuestion}</p>`;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// starts the game when #start game is clicked
+$("#start").click(function() {
+  $("#start").remove();
+  $("#time").html(counter);
+  loadQuestion();
+});
---
+++ assets/javascript/questions.js
@@ -0,0 +1,79 @@
+
+var quizQuestions = [
+//     ["assets/images/NikolaT.jpeg",'0'],
+//     ["assets/images/mustang.jpg",'1'],
+//     ["assets/images/SpaceX.jpg",'2'],
+//     ["assets/images/Model3.jpg",'3'],
+//     ["assets/images/CourtBuilding.jpg",'4'],
+    {
+        
+        //question 1
+        
+      question:
+        "1. Who is this?",
+        choices: [
+        "Nikola Tesla",
+        "Benjamin Franklin",
+        " Thomas Edison",
+        "Napoleon Bonaparte"],
+      correctAnswer: "Nikola Tesla",
+    //   ADD photo
+      
+    }, ////////////////////////////////////////////
+    {
+        //question 2
+      question:
+        "2. What car are these?",
+      choices: [
+        "Nissan Altima",
+        "Ford Mustang",
+        "Dodge Challenger",
+        "Chevrolet Camaro"],
+      correctAnswer: "Ford Mustang",
+    //   ADD photo
+      
+  
+    }, ////////////////////////////////////////////
+    {
+      //question 3
+    question:
+      "3. Who is the face of SpaceX?",
+    choices: [
+      "Niel deGrasse Tyson",
+      "Jon Stewart",
+      "Elon Musk",
+      "Leonardo DiCaprio"],
+    correctAnswer: "Elon Musk",
+    //   ADD photo
+    
+    
+  }, ////////////////////////////////////////////
+  {
+      //question 4
+    question:
+      "4. What Model number is this?",
+    choices: [
+      "9",
+      "12",
+      "9",
+      "3"],
+    correctAnswer: "3",
+    //   ADD photo
+    
+  }, ////////////////////////////////////////////
+  {
+      //question 5
+    question:
+      "5. Who won numerous Supreme Court case on curing cancer?",
+    choices: [
+      "Andrew Jackson",
+      "Lisa Lopes AKA Left Eye",
+      "Ermias Joseph Asghedom AKA Nipsey Hussle",
+      "Alfredo Darrington Bowman AKA DR.Sebi"],
+    correctAnswer: "Alfredo Darrington Bowman AKA DR.Sebi",
+    //   ADD photo
+    
+  },
+  
+  ];
+  
