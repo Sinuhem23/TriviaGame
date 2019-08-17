9e35fb995f4aa83155ea12a148949b3a6476861b
sinuhe Montero <sinuhem23@gmail.com>
2019-08-17 00:05:06 -0400

    TriviaGame

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,36 +13,30 @@
 
 
 
+function countdown() {
+  var seconds = 60;
+  function tick() {
+      var counter = document.getElementById("counter");
+      seconds--;
+      counter.innerHTML = "0:" + (seconds < 10 ? "0" : "") + String(seconds);
+      if( seconds > 0 ) {
+          setTimeout(tick, 1000);
 
-// Set the timer we're counting down to
-var countDown = new Date("60000").getTime();
+      }else if ( seconds < 10 ){
+        document.getElementById("counter").style.color = 'red';
 
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  var distance = countDown - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result 
-  document.getElementById("displayTimer").innerHTML = seconds + "s ";
-
-  // If the count down is finished, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("displayTimer").innerHTML = "All Done";
-
-   
-        
+      }
+      
+      else {
+          alert("Time is UP");
+      }
   }
+  tick();
+  
+}
 
-}, 1000);
+// start the countdown
+countdown();
 
 //  // Hide the directions
 //  directionsText.textContent = "";
